f7c8e35902fae1e3d468e3f89fc6516a61fbcad1
ajs1109 <78317286+ajs1109@users.noreply.github.com>
2026-06-17 01:27:11 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://ajiteshsr615:<your-pass>@cluster0.ubltt.mongodb.net/RapidKeys?retryWrites=true&w=majority&appName=Cluster0";
-export const JWT_SECRET = process.env.JWT_SECRET || (() => { throw new Error('JWT_SECRET env var is not set'); })();
+export const JWT_SECRET = typeof window !== 'undefined' ? '' : (process.env.JWT_SECRET || (() => { throw new Error('JWT_SECRET env var is not set'); })());
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || 'your-secret-key';
 export const REFRESH_SECRET = process.env.REFRESH_SECRET || 'your-refresh-secret';
 export const NODE_ENV = process.env.NODE_ENV || 'development';
